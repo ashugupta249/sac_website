534cbf2dcff31549641dc476108a1c5587a04a7b
Subhalingam D <subhalingam.d@gmail.com>
2020-06-05 10:52:25 +0530

    added SAC page template

---
+++ sac/script.js
@@ -0,0 +1,29 @@
+function vertical_tabs()
+  {
+    $('.container').on("click", ".ux-vertical-tabs .tabs button", function()
+      {
+      var temp_tab = $(this).data('tab');
+      var temp_tab_js = this.getAttribute('data-tab');
+      var temp_content = $('.ux-vertical-tabs .maincontent .tabcontent[data-tab="' + temp_tab + '"]');
+      var temp_content_js = document.querySelector('.ux-vertical-tabs .maincontent .tabcontent[data-tab="' + temp_tab_js + '"]');
+      var temp_content_active_js = document.querySelector('.ux-vertical-tabs .maincontent .tabcontent.active');
+
+      $('.ux-vertical-tabs .tabs button').removeClass('active');
+      $('.ux-vertical-tabs .tabs button[data-tab="' + temp_tab + '"]').addClass('active');
+
+      var animation_start = anime({ duration: 400, targets: temp_content_active_js, opacity:[1,0], translateX: [0,'100%'], easing: 'easeInOutCubic',
+      complete: function()
+        {
+          $('.ux-vertical-tabs .maincontent .tabcontent').removeClass('active');
+        temp_content.addClass('active');
+        var animation_end = anime({ duration: 400, targets: temp_content_js, opacity:[0,1], translateX: ['100%','0'], easing: 'easeInOutCubic' });
+          }
+      });
+      });
+    }
+
+
+$(function() 
+  {
+  vertical_tabs();
+  });
