772d4674606d9f75c8223360b6b3aa93571dd6f3
Adarsh Jain <lemme@pop-os.localdomain>
2020-06-11 16:17:43 +0530

    mobile view navbar - closes on clicking again on dropdown menus

--- navbar/script-nav.js
+++ navbar/script-nav.js
@@ -1,8 +1,25 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
+    // var a = x.getElementsByClassName("nav-links")[0];
     if (x.className === "topnav") {
       x.className += " responsive";
+      // var h = a.clientHeight;
+      // a.setAttribute("style","hieght:0px");
+      // console.log(a.clientHeight);
     } else {
       x.className = "topnav";
     }
   }
+
+function dropdown(a){
+  // console.log(a);
+  if(window.innerWidth<600){
+    var e = a.getElementsByClassName("dropdown-content")[0];
+    if(e.getAttribute("style") == "display:block"){
+      e.setAttribute("style","");
+    }
+    else{
+      e.setAttribute("style","display:block");
+    }
+  }
+}
