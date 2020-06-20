fd2e5fa1e2fef4c7b5e552e3a057cb1800734e69
Adarsh Jain <ee3180525@iitd.ac.in>
2020-06-20 15:05:46 +0530

    scroll to top

---
+++ footer/scroll-top.js
@@ -0,0 +1,22 @@
+var stepTime = 20;
+var docBody = document.body;
+var focElem = document.documentElement;
+
+var scrollAnimationStep = function (initPos, stepAmount) {
+    var newPos = initPos - stepAmount > 0 ? initPos - stepAmount : 0;
+
+    docBody.scrollTop = focElem.scrollTop = newPos;
+
+    newPos && setTimeout(function () {
+        scrollAnimationStep(newPos, stepAmount);
+    }, stepTime);
+}
+
+var scrollTopAnimated = function (speed) {
+    var topOffset = docBody.scrollTop || focElem.scrollTop;
+    var stepAmount = topOffset;
+
+    speed && (stepAmount = (topOffset * stepTime)/speed);
+
+    scrollAnimationStep(topOffset, stepAmount);
+};
