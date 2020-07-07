3eedf2ddf0fd007c978eb7b8ed2929e3ebf282c1
Adarsh Jain <ee3180525@iitd.ac.in>
2020-07-07 21:14:40 +0530

    arrow added to tab dropdown

--- sac/script-tab.js
+++ sac/script-tab.js
@@ -1,11 +1,14 @@
 var tab_col = $(".tabs")[0];
-var tab_toggle = $(".tab-toggle")[0];
+var tabtoggle = $(".tab-toggle")[0];
+var downarrow = $(".tab-toggle i")[0];
 
 var tab_show = function(){
 	tab_col.setAttribute("style","transform:scaleY(1)");
+	downarrow.setAttribute("style","transform:rotateZ(180deg)")
 }
 var tab_hide = function(){
 	tab_col.setAttribute("style","@media (max-width: 50rem) {transform:scaleY(0)}");
+	downarrow.setAttribute("style","transform:rotateZ(0deg)")
 }
 
 var tab_toggle = function(){
