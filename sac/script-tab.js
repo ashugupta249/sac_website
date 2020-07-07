77c52e97e9b391251991854061e01d8de3117f9e
Adarsh Jain <ee3180525@iitd.ac.in>
2020-07-07 21:18:29 +0530

    fix for javascript multiple files

--- sac/script-tab.js
+++ sac/script-tab.js
@@ -1,22 +1,22 @@
-var tab_col = $(".tabs")[0];
-var tabtoggle = $(".tab-toggle")[0];
-var downarrow = $(".tab-toggle i")[0];
+// var tab_col = $(".tabs")[0];
+// var tabtoggle = $(".tab-toggle")[0];
+// var downarrow = $(".tab-toggle i")[0];
 
-var tab_show = function(){
-	tab_col.setAttribute("style","transform:scaleY(1)");
-	downarrow.setAttribute("style","transform:rotateZ(180deg)")
-}
-var tab_hide = function(){
-	tab_col.setAttribute("style","@media (max-width: 50rem) {transform:scaleY(0)}");
-	downarrow.setAttribute("style","transform:rotateZ(0deg)")
-}
+// var tab_show = function(){
+// 	tab_col.setAttribute("style","transform:scaleY(1)");
+// 	downarrow.setAttribute("style","transform:rotateZ(180deg)")
+// }
+// var tab_hide = function(){
+// 	tab_col.setAttribute("style","@media (max-width: 50rem) {transform:scaleY(0)}");
+// 	downarrow.setAttribute("style","transform:rotateZ(0deg)")
+// }
 
-var tab_toggle = function(){
-	state = tab_col.style.transform;
-	if(state == "scaleY(1)"){
-		tab_hide();
-	}
-	else{
-		tab_show();
-	}
-}
+// var tab_toggle = function(){
+// 	state = tab_col.style.transform;
+// 	if(state == "scaleY(1)"){
+// 		tab_hide();
+// 	}
+// 	else{
+// 		tab_show();
+// 	}
+// }
