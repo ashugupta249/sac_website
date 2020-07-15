522b93ad331cd8d33c888ae287333757364a4118
Subhalingam D <subhalingam.d@gmail.com>
2020-07-15 19:20:48 +0530

    remove extra lines in script

--- sac/script.js
+++ sac/script.js
@@ -1,14 +1,10 @@
 /*
   Contents:
     * Main scripts for theme
-
 */
-
-
 var tab_col = $(".tabs")[0];
 var tabtoggle = $(".tab-toggle")[0];
 var downarrow = $(".tab-toggle i")[0];
-
 var tab_show = function(){
   tabtoggle.classList.add("tab-toggle-active");
   tab_col.setAttribute("style","transform:scaleY(1)");
@@ -26,7 +22,6 @@ var tab_hide = function(){
   tabtoggle.setAttribute("style","margin-top:0");
   // downarrow.setAttribute("style","transform:rotateZ(0deg)")
 }
-
 var tab_toggle = function(){
   // alert("Clicked");
   state = tab_col.style.transform;
@@ -37,11 +32,6 @@ var tab_toggle = function(){
     tab_show();
   }
 }
-
-
-
-
-
 $('.container').on("click", ".ux-vertical-tabs .tabs button", function(){
     $('html, body').animate({
         scrollTop: $('.ux-vertical-tabs .maincontent .tabcontent[data-tab="' + $(this).data('tab') + '"]').offset().top - $('header').outerHeight() + 2
@@ -49,122 +39,83 @@ $('.container').on("click", ".ux-vertical-tabs .tabs button", function(){
     tab_hide();
     return false;
 });
-
-
 $(window).scroll(function (e){
     var q = $(".ux-vertical-tabs .tabs button:not(.empty)");
     var len = q.length;
-
     var i = 0;
-
     for (i = 0; i < len; i++) {
       q.eq(i).removeClass("active");
     }
-
     var top = $(window).scrollTop() + $('header').outerHeight();
-
     for (i = 0; i < len; i++){
       if (top < $('.ux-vertical-tabs .maincontent .tabcontent[data-tab="' + q.eq(i).data('tab') + '"]').offset().top)
         break;
     }
-
     // Handle i = 0 case
     if (i==0)
       i=1;
-
     q.eq(i-1).addClass("active");
-
-    
 });
-
-
 /***************************** Initiatives ********************************/
 /* By Subhalingam */
-
 $(".init-item").click(function(){
-
   var data = $(this).data("init-index");
   var book_item = $(this);
   var book_container = book_item.parent();
-  
-  
-
   if ($(this).hasClass('init-item-active')){
     $('.init-index-container[data-init-index="'+data+'"]').slideUp(function(){
       book_item.removeClass('init-item-active');
       book_container.css("white-space", "normal");  
     });
-    
   }
   else{
     $(this).siblings().removeClass("init-item-active");
     $(this).parent().parent().find('.init-index-container:not([data-init-index="'+data+'"])').css("display","none");      
-    
     book_container.css("white-space", "nowrap");
     book_item.addClass('init-item-active');
-
     if ($(window).scrollTop() + $('header').height() + 5 > book_container.offset().top){
       //alert('triggered');
       $('html,body').animate({
         scrollTop: book_container.offset().top - $('header').height() - 5
        }, 500);
     } 
-
-
     $('.init-index-container[data-init-index="'+data+'"]').slideDown(function(){
       book_container.animate({
             scrollLeft: book_item.offset().left - 50
         }, 500);
     });
   } 
-
-
-
-  
-  
 })
-
 $(".init-index-container-close").click(function(){
   $(this).parent().slideUp(function(){
     $(this).parent().parent().find(".init-container").css("white-space", "normal");
     $(this).parent().parent().find(".init-item").removeClass("init-item-active");
   }); 
-
 })
-
 $(".init-item-read").click(function(e){
   e.preventDefault();
 })
-
-
 /********************************* Policy changes ********************************/
 /*
 Reference: http://jsfiddle.net/BB3JK/47/
 */
-
 $('select').each(function(){
     var $this = $(this), numberOfOptions = $(this).children('option').length;
-  
     $this.addClass('select-hidden'); 
     $this.wrap('<div class="select"></div>');
     $this.after('<div class="select-styled"></div>');
-
     var $styledSelect = $this.next('div.select-styled');
     $styledSelect.text($this.children('option').eq(0).text());
-  
     var $list = $('<ul />', {
         'class': 'select-options'
     }).insertAfter($styledSelect);
-  
     for (var i = 0; i < numberOfOptions; i++) {
         $('<li />', {
             text: $this.children('option').eq(i).text(),
             rel: $this.children('option').eq(i).val()
         }).appendTo($list);
     }
-  
     var $listItems = $list.children('li');
-  
     $styledSelect.click(function(e) {
         e.stopPropagation();
         $('div.select-styled.active').not(this).each(function(){
@@ -172,7 +123,6 @@ $('select').each(function(){
         });
         $(this).toggleClass('active').next('ul.select-options').toggle();
     });
-  
     $listItems.click(function(e) {
         e.stopPropagation();
         $styledSelect.text($(this).text()).removeClass('active');
@@ -180,37 +130,26 @@ $('select').each(function(){
         $list.hide();
         //console.log($this.val());
     });
-  
     $(document).click(function() {
         $styledSelect.removeClass('active');
         $list.hide();
     });
-
 });
-
-
 function policy_year_filter(){
     var y = $('.select-styled').html();
     $('.accordion>div:not([data-year='+y+'])').fadeOut(function(){
         $('.accordion>div[data-year='+y+']').fadeIn();
     });  
 }
-
 $('.select-styled').on('DOMSubtreeModified',function(){
    policy_year_filter();
 });
-
-
 policy_year_filter();
-
 /*****************************  Minutes of Meet ********************************/
-
 $(document).ready(function($){
   var timelines = $('.cd-horizontal-timeline'),
     eventsMinDistance = 60;
-
   (timelines.length > 0) && initTimeline(timelines);
-
   function initTimeline(timelines) {
     timelines.each(function(){
       var timeline = $(this),
@@ -224,14 +163,12 @@ $(document).ready(function($){
       timelineComponents['eventsMinLapse'] = minLapse(timelineComponents['timelineDates']);
       timelineComponents['timelineNavigation'] = timeline.find('.cd-timeline-navigation');
       timelineComponents['eventsContent'] = timeline.children('.events-content');
-
       //assign a left postion to the single events along the timeline
       setDatePosition(timelineComponents, eventsMinDistance);
       //assign a width to the timeline
       var timelineTotWidth = setTimelineWidth(timelineComponents, eventsMinDistance);
       //the timeline has been initialize - show it
       timeline.addClass('loaded');
-
       //detect click on the next arrow
       timelineComponents['timelineNavigation'].on('click', '.next', function(event){
         event.preventDefault();
@@ -251,7 +188,6 @@ $(document).ready(function($){
         updateFilling($(this), timelineComponents['fillingLine'], timelineTotWidth);
         updateVisibleContent($(this), timelineComponents['eventsContent']);
       });
-
       //on swipe, show next/prev event content
       timelineComponents['eventsContent'].on('swipeleft', function(){
         var mq = checkMQ();
@@ -261,7 +197,6 @@ $(document).ready(function($){
         var mq = checkMQ();
         ( mq == 'mobile' ) && showNewContent(timelineComponents, timelineTotWidth, 'prev');
       });
-
       //keyboard navigation
       $(document).keyup(function(event){
         if(event.which=='37' && elementInViewport(timeline.get(0)) ) {
@@ -272,7 +207,6 @@ $(document).ready(function($){
       });
     });
   }
-
   function updateSlide(timelineComponents, timelineTotWidth, string) {
     //retrieve translateX value of timelineComponents['eventsWrapper']
     var translateValue = getTranslateValue(timelineComponents['eventsWrapper']),
@@ -282,16 +216,13 @@ $(document).ready(function($){
       ? translateTimeline(timelineComponents, translateValue - wrapperWidth + eventsMinDistance, wrapperWidth - timelineTotWidth)
       : translateTimeline(timelineComponents, translateValue + wrapperWidth - eventsMinDistance);
   }
-
   function showNewContent(timelineComponents, timelineTotWidth, string) {
     //go from one event to the next/previous one
     var visibleContent =  timelineComponents['eventsContent'].find('.selected'),
       newContent = ( string == 'next' ) ? visibleContent.next() : visibleContent.prev();
-
     if ( newContent.length > 0 ) { //if there's a next/prev event - show it
       var selectedDate = timelineComponents['eventsWrapper'].find('.selected'),
         newEvent = ( string == 'next' ) ? selectedDate.parent('li').next('li').children('a') : selectedDate.parent('li').prev('li').children('a');
-      
       updateFilling(newEvent, timelineComponents['fillingLine'], timelineTotWidth);
       updateVisibleContent(newEvent, timelineComponents['eventsContent']);
       newEvent.addClass('selected');
@@ -300,7 +231,6 @@ $(document).ready(function($){
       updateTimelinePosition(string, newEvent, timelineComponents, timelineTotWidth);
     }
   }
-
   function updateTimelinePosition(string, event, timelineComponents, timelineTotWidth) {
     //translate timeline to the left/right according to the position of the selected event
     var eventStyle = window.getComputedStyle(event.get(0), null),
@@ -308,12 +238,10 @@ $(document).ready(function($){
       timelineWidth = Number(timelineComponents['timelineWrapper'].css('width').replace('px', '')),
       timelineTotWidth = Number(timelineComponents['eventsWrapper'].css('width').replace('px', ''));
     var timelineTranslate = getTranslateValue(timelineComponents['eventsWrapper']);
-
         if( (string == 'next' && eventLeft > timelineWidth - timelineTranslate) || (string == 'prev' && eventLeft < - timelineTranslate) ) {
           translateTimeline(timelineComponents, - eventLeft + timelineWidth/2, timelineWidth - timelineTotWidth);
         }
   }
-
   function translateTimeline(timelineComponents, value, totWidth) {
     var eventsWrapper = timelineComponents['eventsWrapper'].get(0);
     value = (value > 0) ? 0 : value; //only negative translate value
@@ -323,7 +251,6 @@ $(document).ready(function($){
     (value == 0 ) ? timelineComponents['timelineNavigation'].find('.prev').addClass('inactive') : timelineComponents['timelineNavigation'].find('.prev').removeClass('inactive');
     (value == totWidth ) ? timelineComponents['timelineNavigation'].find('.next').addClass('inactive') : timelineComponents['timelineNavigation'].find('.next').removeClass('inactive');
   }
-
   function updateFilling(selectedEvent, filling, totWidth) {
     //change .filling-line length according to the selected event
     var eventStyle = window.getComputedStyle(selectedEvent.get(0), null),
@@ -333,7 +260,6 @@ $(document).ready(function($){
     var scaleValue = eventLeft/totWidth;
     setTransformValue(filling.get(0), 'scaleX', scaleValue);
   }
-
   function setDatePosition(timelineComponents, min) {
     for (i = 0; i < timelineComponents['timelineDates'].length; i++) { 
         var distance = daydiff(timelineComponents['timelineDates'][0], timelineComponents['timelineDates'][i]),
@@ -341,7 +267,6 @@ $(document).ready(function($){
         timelineComponents['timelineEvents'].eq(i).css('left', distanceNorm*min+'px');
     }
   }
-
   function setTimelineWidth(timelineComponents, width) {
     var timeSpan = daydiff(timelineComponents['timelineDates'][0], timelineComponents['timelineDates'][timelineComponents['timelineDates'].length-1]),
       timeSpanNorm = timeSpan/timelineComponents['eventsMinLapse'],
@@ -349,16 +274,13 @@ $(document).ready(function($){
       totalWidth = timeSpanNorm*width;
     timelineComponents['eventsWrapper'].css('width', totalWidth+'px');
     updateFilling(timelineComponents['timelineEvents'].eq(0), timelineComponents['fillingLine'], totalWidth);
-  
     return totalWidth;
   }
-
   function updateVisibleContent(event, eventsContent) {
     var eventDate = event.data('date'),
       visibleContent = eventsContent.find('.selected'),
       selectedContent = eventsContent.find('[data-date="'+ eventDate +'"]'),
       selectedContentHeight = selectedContent.height();
-
     if (selectedContent.index() > visibleContent.index()) {
       var classEnetering = 'selected enter-right',
         classLeaving = 'leave-left';
@@ -366,7 +288,6 @@ $(document).ready(function($){
       var classEnetering = 'selected enter-left',
         classLeaving = 'leave-right';
     }
-
     selectedContent.attr('class', classEnetering);
     visibleContent.attr('class', classLeaving).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
       visibleContent.removeClass('leave-right leave-left');
@@ -374,11 +295,9 @@ $(document).ready(function($){
     });
     eventsContent.css('height', selectedContentHeight+'px');
   }
-
   function updateOlderEvents(event) {
     event.parent('li').prevAll('li').children('a').addClass('older-event').end().end().nextAll('li').children('a').removeClass('older-event');
   }
-
   function getTranslateValue(timeline) {
     var timelineStyle = window.getComputedStyle(timeline.get(0), null),
       timelineTranslate = timelineStyle.getPropertyValue("-webkit-transform") ||
@@ -386,7 +305,6 @@ $(document).ready(function($){
             timelineStyle.getPropertyValue("-ms-transform") ||
             timelineStyle.getPropertyValue("-o-transform") ||
             timelineStyle.getPropertyValue("transform");
-
         if( timelineTranslate.indexOf('(') >=0 ) {
           var timelineTranslate = timelineTranslate.split('(')[1];
         timelineTranslate = timelineTranslate.split(')')[0];
@@ -395,10 +313,8 @@ $(document).ready(function($){
         } else {
           var translateValue = 0;
         }
-
         return Number(translateValue);
   }
-
   function setTransformValue(element, property, value) {
     element.style["-webkit-transform"] = property+"("+value+")";
     element.style["-moz-transform"] = property+"("+value+")";
@@ -406,7 +322,6 @@ $(document).ready(function($){
     element.style["-o-transform"] = property+"("+value+")";
     element.style["transform"] = property+"("+value+")";
   }
-
   //based on http://stackoverflow.com/questions/542938/how-do-i-get-the-number-of-days-between-two-dates-in-javascript
   function parseDate(events) {
     var dateArrays = [];
@@ -417,7 +332,6 @@ $(document).ready(function($){
     });
       return dateArrays;
   }
-
   function parseDate2(events) {
     var dateArrays = [];
     events.each(function(){
@@ -438,11 +352,9 @@ $(document).ready(function($){
     });
       return dateArrays;
   }
-
   function daydiff(first, second) {
       return Math.round((first-second));
   }
-
   function minLapse(dates) {
     //determine the minimum distance among events
     var dateDistances = [];
@@ -452,7 +364,6 @@ $(document).ready(function($){
     }
     return Math.min.apply(null, dateDistances);
   }
-
   /*
     How to tell if a DOM element is visible in the current viewport?
     http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
@@ -462,13 +373,11 @@ $(document).ready(function($){
     var left = el.offsetLeft;
     var width = el.offsetWidth;
     var height = el.offsetHeight;
-
     while(el.offsetParent) {
         el = el.offsetParent;
         top += el.offsetTop;
         left += el.offsetLeft;
     }
-
     return (
         top < (window.pageYOffset + window.innerHeight) &&
         left < (window.pageXOffset + window.innerWidth) &&
@@ -476,7 +385,6 @@ $(document).ready(function($){
         (left + width) > window.pageXOffset
     );
   }
-
   function checkMQ() {
     //check if mobile or desktop device
     return window.getComputedStyle(document.querySelector('.cd-horizontal-timeline'), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "");
