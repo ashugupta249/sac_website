8b61f0f32363913bba82222db697af26faab7d34
Subhalingam D <subhalingam.d@gmail.com>
2020-07-17 15:30:34 +0530

    Redesign Boards dropdown (#3)
    * update
    
    * Fix case in title while loading
    
    * Update dropdown
    
    * Update index.html : remove links!
    
    * border color and margin changes
    
    * fix fading while clicking on dropdown
    
    * Update script-drop.js : fadeout fixed
    
    * round borders & z-index change in dropdown
    
    Co-authored-by: Mr Adarsh Jain <46607038+adarsh-ee3180525@users.noreply.github.com>

--- boards/script-drop.js
+++ boards/script-drop.js
@@ -1,151 +1,75 @@
-// For dropdown
-function create_custom_dropdowns() {
-    $('select').each(function (i, select) {
-        if (!$(this).next().hasClass('dropdown-select')) {
-            $(this).after('<div class="dropdown-select wide ' + ($(this).attr('class') || '') + '" tabindex="0"><span class="current"></span><div class="list"><ul></ul></div></div>');
-            var dropdown = $(this).next();
-            var options = $(select).find('option');
-            var selected = $(this).find('option:selected');
-            dropdown.find('.current').html(selected.data('display-text') || selected.text());
-            options.each(function (j, o) {
-                var display = $(o).data('display-text') || '';
-                dropdown.find('ul').append('<li class="option ' + ($(o).is(':selected') ? 'selected' : '') + '" data-value="' + $(o).val() + '" data-display-text="' + display + '">' + $(o).text() + '</li>');
-            });
-        }
+
+$('select').each(function(){
+    var $this = $(this), numberOfOptions = $(this).children('option').length;
+  
+    $this.addClass('select-hidden'); 
+    $this.wrap('<div class="select"></div>');
+    $this.after('<div class="select-styled"></div>');
+
+    var $styledSelect = $this.next('div.select-styled');
+    $styledSelect.text($this.children('option').eq(0).text());
+  
+    var $list = $('<ul />', {
+        'class': 'select-options'
+    }).insertAfter($styledSelect);
+  
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+  
+    var $listItems = $list.children('li');
+  
+    $styledSelect.click(function(e) {
+        e.stopPropagation();
+        $('div.select-styled.active').not(this).each(function(){
+            $(this).removeClass('active').next('ul.select-options').hide();
+        });
+        $(this).toggleClass('active').next('ul.select-options').toggle();
+    });
+  
+    $listItems.click(function(e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+        //console.log($this.val());
+    });
+  
+    $(document).click(function() {
+        $styledSelect.removeClass('active');
+        $list.hide();
     });
 
-    $('.dropdown-select ul').before('<div class="dd-search"><input id="txtSearchValue" autocomplete="off" onkeyup="filter()" class="dd-searchbox" type="text"></div>');
+});
+
+
+$.toggler = function(val){ 
+  $('.container').fadeOut(function(){
+    $(".container").html("");
+    $(".container").load("./partials/"+val.toLowerCase()+".html .ux-vertical-tabs"); 
+    $("title").html("Boards - "+val);
+    $('.container').fadeIn();
+    history.replaceState(undefined, undefined, "#"+val.toLowerCase());
+ }) 
 }
 
-// Event listeners
+$.hashChanger = function(){
+  var val = window.location.hash.substring(1).toLowerCase();
+  var univ_set = ['bsw','bsp','brca','bsa','bhm'];
+  val = (univ_set.indexOf(val) > -1) ? val : 'bsw';
+  val = val.toUpperCase();
+  $('#drop .select-styled').html(val);
+ 
+  $.toggler(val);
+}
 
-// Open/close
-$(document).on('click', '.dropdown-select', function (event) {
-    if($(event.target).hasClass('dd-searchbox')){
-        return;
-    }
-    $('.dropdown-select').not($(this)).removeClass('open');
-    $(this).toggleClass('open');
-    if ($(this).hasClass('open')) {
-        $(this).find('.option').attr('tabindex', 0);
-        $(this).find('.selected').focus();
-    } else {
-        $(this).find('.option').removeAttr('tabindex');
-        $(this).focus();
-    }
-});
-
-// Close when clicking outside
-$(document).on('click', function (event) {
-    if ($(event.target).closest('.dropdown-select').length === 0) {
-        $('.dropdown-select').removeClass('open');
-        $('.dropdown-select .option').removeAttr('tabindex');
-    }
-    event.stopPropagation();
-});
-
-function filter(){
-    var valThis = $('#txtSearchValue').val();
-    $('.dropdown-select ul > li').each(function(){
-     var text = $(this).text();
-        (text.toLowerCase().indexOf(valThis.toLowerCase()) > -1) ? $(this).show() : $(this).hide();         
-   });
-};
-// Search
-
-// Option click
-$(document).on('click', '.dropdown-select .option', function (event) {
-    $(this).closest('.list').find('.selected').removeClass('selected');
-    $(this).addClass('selected');
-    var text = $(this).data('display-text') || $(this).text();
-    $(this).closest('.dropdown-select').find('.current').text(text);
-    $(this).closest('.dropdown-select').prev('select').val($(this).data('value')).trigger('change');
-});
-
-// Keyboard events
-$(document).on('keydown', '.dropdown-select', function (event) {
-    var focused_option = $($(this).find('.list .option:focus')[0] || $(this).find('.list .option.selected')[0]);
-    // Space or Enter
-    //if (event.keyCode == 32 || event.keyCode == 13) {
-    if (event.keyCode == 13) {
-        if ($(this).hasClass('open')) {
-            focused_option.trigger('click');
-        } else {
-            $(this).trigger('click');
-        }
-        return false;
-        // Down
-    } else if (event.keyCode == 40) {
-        if (!$(this).hasClass('open')) {
-            $(this).trigger('click');
-        } else {
-            focused_option.next().focus();
-        }
-        return false;
-        // Up
-    } else if (event.keyCode == 38) {
-        if (!$(this).hasClass('open')) {
-            $(this).trigger('click');
-        } else {
-            var focused_option = $($(this).find('.list .option:focus')[0] || $(this).find('.list .option.selected')[0]);
-            focused_option.prev().focus();
-        }
-        return false;
-        // Esc
-    } else if (event.keyCode == 27) {
-        if ($(this).hasClass('open')) {
-            $(this).trigger('click');
-        }
-        return false;
-    }
-});
-
-$(document).ready(function () {
-    /* if (window.location.hash) {
-        $('#drop select option[value='+window.location.hash.substring(1)+']').attr('selected','selected');
-    }
-    */
-    
-    create_custom_dropdowns();
+$.hashChanger(); 
 
 
-    
-    $.toggler = function(val){ 
-      $('.container').fadeOut(function(){
-        $(".container").html("");
-        $(".container").load("./partials/"+val+".html .ux-vertical-tabs"); 
-        $("title").html("Boards - "+val.toUpperCase());
-        $('.container').fadeIn();
-        history.replaceState(undefined, undefined, "#"+val);
-     }) 
-    }
-
-    $.hashChanger = function(){
-      var val = window.location.hash.substring(1).toLowerCase();
-      var univ_set = ['bsw','bsp','brca'];
-      val = (univ_set.indexOf(val) > -1) ? val : 'bsw';
-      $('.dropdown-select .option[data-value=' + val + ']').trigger("click");
-      $(document).trigger('click');
-      $.toggler(val);
-    }
-
-    $.hashChanger();        
-
-    /*
-    if (window.location.hash) {
-          // alert(window.location.hash.substring(1));
-          $.toggler(window.location.hash.substring(1));
-     }
-     else{
-          $.toggler("bsw");
-     }
-     
-     */
-
-    $('#drop select').on('change', function() {
-      // alert('triggered '+val);
-      $.toggler($(this).val());
-    })
-
-
+$('#drop .select-styled').on('DOMSubtreeModified',function(){
+   if ($('#drop .select-styled').html() != window.location.hash.substring(1))
+        $.toggler($('#drop .select-styled').html());
 });
