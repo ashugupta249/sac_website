ce994a34bb3362973b477598d733f538017375be
Adarsh Jain <ee3180525@iitd.ac.in>
2020-07-18 11:24:22 +0530

    fadeout-fadein on clicking fixed

--- boards/script-drop.js
+++ boards/script-drop.js
@@ -70,6 +70,9 @@ $.hashChanger();
 
 
 $('#drop .select-styled').on('DOMSubtreeModified',function(){
-   if ($('#drop .select-styled').html() != window.location.hash.substring(1))
-        $.toggler($('#drop .select-styled').html());
+    if ($('#drop .select-styled').html()){
+        if($('#drop .select-styled').html() != window.location.hash.substring(1).toUpperCase()){
+            $.toggler($('#drop .select-styled').html());
+        }
+    }
 });
