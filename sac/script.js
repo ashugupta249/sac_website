cd32f6f0a3b5d143bb579ee0329e8e68fcc8f94b
Adarsh Jain <ee3180525@iitd.ac.in>
2020-07-18 12:28:12 +0530

    arrow goes below toggle tab if possible

--- sac/script.js
+++ sac/script.js
@@ -10,7 +10,12 @@ var tab_show = function(){
 
   tabtoggle.classList.add("tab-toggle-active");
   tab_col.setAttribute("style","transform:scaleY(1)");
-  tabtoggle.setAttribute("style","margin-top:"+(tab_col.clientHeight - tabtoggle.clientHeight)+"px");
+  if(tabtoggle.scrollHeight + 84 < screen.height){
+    tabtoggle.setAttribute("style","margin-top:"+(tab_col.clientHeight)+"px");
+  }
+  else{
+    tabtoggle.setAttribute("style","margin-top:"+(tab_col.clientHeight - tabtoggle.clientHeight)+"px");
+  }
   // downarrow.setAttribute("style","transform:rotateZ(180deg)")
   $(".dim-caller").addClass("dimmer");
 }
