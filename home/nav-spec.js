ef95ec61a3a44882b722b049f5d2f13b3184e430
Subhalingam D <subhalingam.d@gmail.com>
2020-07-25 11:43:01 +0530

    Navbar animation in all pages (#12)
    * Update nav-spec.js
    
    a to bloack
    
    * Update nav-spec.js
    
    clean
    
    * Update style-nav.css
    
    clean
    
    * change navbar in static/
    
    * dropdown cursor:pointer
    
    * Update nav in sac
    
    * remove meyer-reset in sac
    
    * trigger click events from js
    
    * trigger click events from js in home
    
    * main margin-top 49px fix
    
    * Update boards
    
    * update clubs
    
    * make main margin-top:0
    
    avoid problems while merging

--- home/nav-spec.js
+++ home/nav-spec.js
@@ -1,64 +1,45 @@
-function myFunction() {
-    var x = $("#myTopnav");
+$('#toggle-nav').click(function(){
+    //var x = $("#myTopnav");
     // var a = x.getElementsByClassName("nav-links")[0];
     if (!$("#myTopnav").hasClass("responsive")) {
-      $(".nav-links").children().slideDown();
+      $(".nav-links").children().slideDown(function(){
+        $(this).css("display", "block");
+      });
       $("#myTopnav").addClass("responsive");
       //y.className += "hidden";
-      
-      // var h = a.clientHeight;
-      // a.setAttribute("style","hieght:0px");
-      // console.log(a.clientHeight);
     } else {
       $(".nav-links").children().slideUp();
       $(".dropdown-content").children().slideUp();
       $("#myTopnav").removeClass("responsive");
     }
-}
-
-function dropdown(a){
-  // console.log(a);
-  // if(window.innerWidth<600){
-
-  //   x = a.getElementsByClassName("dropdown-content")[0];
-  //   var all_dropdown = document.getElementsByClassName("dropdown");
-  //   for(i=0;i<all_dropdown.length;i++){
-  //     e = all_dropdown[i].getElementsByClassName("dropdown-content")[0];
-      
-  //     if(x!=e){
-  //     e.setAttribute("style","");}
-  //   }
-
-    
-  //   if(x.getAttribute("style") == "display:block"){
-  //     x.setAttribute("style","");
-  //   }
-  //   else{
-  //     x.setAttribute("style","display:block");
-  //   }
-  // }
+});
+$('#myTopnav .dropdown').click(function(){
+  var a = $(this);
   x = $(a).find(".dropdown-content");
-  
   $(a).find(".dropdown-content").width($(a).width());
   $(".dropdown-content").toArray().forEach((value) => {
     if(value!=x.toArray()[0]){
       $(value).children().slideUp();
     }
   }); 
-  $(a).find(".dropdown-content").children().slideToggle();
-  //alert($(a).width());
-}
+  $(a).find(".dropdown-content").children().slideToggle(function(){
+    if ($(this).is(":visible"))
+      $(this).css("display", "block");
+  });
+})
 
 var curr= $('#toggle-nav').css('display');
-
 $(window).resize(function(){
   var x = document.getElementById("myTopnav");
-  
    /// alert($('#toggle-nav').css('display'));
   if ($('#toggle-nav').css('display')!=curr){
     curr = $('#toggle-nav').css('display');
     if ($('#toggle-nav').css('display')==='none'){
-      $(".nav-links").children().slideDown();
+      $(".nav-links").children().slideDown(function(){
+        if ($(this).is(":visible"))
+          $(this).css("display", "block");
+      });
+      $(".dropdown-content").children().slideUp();
       //$(".dropdown-content").children().slideDown();
       x.className = "topnav";
     }
@@ -71,7 +52,6 @@ $(window).resize(function(){
 });
 
 $(document).scroll(function() {
-    // alert();
  logo_visibility_toggler();
 })
 
