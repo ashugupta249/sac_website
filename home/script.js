cbe120b28724095d3c7806c8a26977372e201e59
Subhalingam D <subhalingam.d@gmail.com>
2020-07-25 17:13:47 +0530

    fix lead next top

--- home/script.js
+++ home/script.js
@@ -1,6 +1,6 @@
 // Scroll to first element
 $('#lead-down span').click(function() {
-    var scrollDistance = $('#lead').next().offset().top;
+    var scrollDistance = $('#lead').next().offset().top - $('header').height() + 5;
     $('html, body').animate({
         scrollTop: scrollDistance + 'px'
     }, 500);
