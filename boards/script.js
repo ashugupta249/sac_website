d38298d4586d6ba9d349ccb14707f3bec0136014
Subhalingam D <subhalingam.d@gmail.com>
2020-09-05 16:14:27 +0530

    scroll top when board change

--- boards/script.js
+++ boards/script.js
@@ -153,6 +153,7 @@ $.toggler = function(val){
   //$('.container').fadeOut(function(){
     //$(".container").html("");
     $(".container").load("./partials/"+val.toLowerCase()+".html .ux-vertical-tabs",function(){
+      $(window).scrollTop(0);
       var timelines = $('.cd-horizontal-timeline');
       (timelines.length > 0) && initTimeline(timelines);
     }); 
